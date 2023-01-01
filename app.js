cc91592af01ae2d3ec6423e977bb40f4503bdc82
prashanta <pbt9840@gmail.com>
2023-01-01 23:28:40 +0545

    next and previous page added!

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const app = express();
 const productData =require('./data/productData.json');
 
 app.get('/products',(req,res) =>{
-    const {page,limit} =req.query;
+    const page =parseInt(req.query.page);
+    const limit =parseInt(req.query.limit);
     startIndex=(page-1)*limit;
     endIndex = page*limit;
-    const paginateData= productData.slice(startIndex,endIndex);
-    res.json(paginateData)
+    const results ={}
+    if(endIndex<productData.length)
+    results.next={
+        page:page+1,
+        limit:limit
+    }
+    if(startIndex>0)
+    results.previous={
+        page:page-1,
+        limit:limit
+    }
+    results.results= productData.slice(startIndex,endIndex);
+    res.json(results)
 })
 
 app.listen(8080,(err)=>{
