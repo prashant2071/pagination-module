44e33ae53cd518d411c9752d450f84f040cef1b8
prashanta <pbt9840@gmail.com>
2023-01-01 23:43:57 +0545

    users route added

--- app.js
+++ app.js
@@ -3,22 +3,45 @@ const app = express();
 const productData =require('./data/productData.json');
 const userData = require('./data/userData.json')
 
+
+app.get('/users',(req,res) =>{
+    const page =parseInt(req.query.page);
+    const limit =parseInt(req.query.limit);
+    startIndex=(page-1)*limit;
+    endIndex = page*limit;
+    const results ={}
+    if(endIndex<userData.length){
+    results.next={
+        page:page+1,
+        limit:limit
+    }}
+
+    if(startIndex>0){
+    results.previous={
+        page:page-1,
+        limit:limit
+    }}
+    results.results= userData.slice(startIndex,endIndex);
+    res.json(results)
+})
+
 app.get('/products',(req,res) =>{
     const page =parseInt(req.query.page);
     const limit =parseInt(req.query.limit);
     startIndex=(page-1)*limit;
     endIndex = page*limit;
     const results ={}
-    if(endIndex<productData.length)
+    if(endIndex<productData.length){
     results.next={
         page:page+1,
         limit:limit
-    }
-    if(startIndex>0)
+    }}
+
+    if(startIndex>0){
     results.previous={
         page:page-1,
         limit:limit
-    }
+    }}
     results.results= productData.slice(startIndex,endIndex);
     res.json(results)
 })
