4d26a4d35016cb0ff4a6e50aefa92c0a7e98e8a1
prashanta <pbt9840@gmail.com>
2023-01-02 23:32:37 +0545

    next value issue resolved

--- paginatedResult.js
+++ paginatedResult.js
@@ -1,24 +1,37 @@
 function paginatedResult(data){
-    return (req,res,next)=>{
+    return async (req,res,next)=>{
     const page =parseInt(req.query.page);
     const limit =parseInt(req.query.limit);
     startIndex=(page-1)*limit;
     endIndex = page*limit;
     const results ={}
-    if(endIndex<data.length){
-    results.next={
-        page:page+1,
-        limit:limit
-    }}
+    // if(endIndex<data.length){
+    // results.next={
+    //     page:page+1,
+    //     limit:limit
+    // }}
+    if(endIndex< await data.count()){
+        results.next={
+            page:page+1,
+            limit:limit
+        }}
 
     if(startIndex>0){
     results.previous={
         page:page-1,
         limit:limit
     }}
-    results.results= data.slice(startIndex,endIndex);
+    // results.results= data.slice(startIndex,endIndex);
+    try{
+    results.results =await data.find().limit(limit).skip(startIndex)
     res.paginatedResult=results;
     next();
+    }
+    catch(err){
+        res.status(500).json({
+            error:err.message
+        })
+    }
 
 }
 }
